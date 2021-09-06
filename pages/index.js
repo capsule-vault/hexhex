78420a41a750838389d8b668d197be7575e2adad
Harry Chang <harry830622@gmail.com>
2021-09-07 00:06:05 +0800

    Adjust styles

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ const Home = () => {
       </Head>
 
       <header
-        className="flex flex-col sm:flex-row justify-between items-center container mx-auto pt-8 sm:py-10"
+        className="flex flex-col sm:flex-row justify-between items-center container mx-auto py-8 text-[26px]"
         style={{ fontFamily: 'Academy Engraved LET' }}
       >
-        <div className="text-4xl">HexHex</div>
-        <div className="flex flex-col sm:flex-row justify-center items-center text-2xl sm:text-4xl">
+        <div className="">HexHex</div>
+        <div className="flex flex-col sm:flex-row justify-center items-center">
           <div>Opensea</div>
           <div className="sm:pl-16">Twitter</div>
           <div className="sm:pl-16">Contract</div>
@@ -23,7 +23,7 @@ const Home = () => {
       <main className="container mx-auto">
         <div className="flex flex-col items-center text-center">
           <h1
-            className="mt-20 text-7xl"
+            className="text-[80px]"
             style={{
               fontFamily: 'Academy Engraved LET',
             }}
@@ -31,32 +31,40 @@ const Home = () => {
             HexHex (beyond colors)
           </h1>
           <p
-            className="mt-12 text-4xl"
+            className="mt-4 text-[26px]"
             style={{
               fontFamily: 'Academy Engraved LET',
             }}
           >
             HexHex (beyond colors) is a collection of 6,666 arrays of 6
-            hexadecimal codes stored on chain. Images and other functionality
-            are intentionally omitted for others to interpret.
+            hexadecimal codes stored on chain.
+            <br />
+            Images and other functionality are intentionally omitted for others
+            to interpret.
           </p>
-          <p className="mt-12 text-lg" style={{ fontFamily: 'Andale Mono' }}>
+          <p className="mt-4 text-[16px]" style={{ fontFamily: 'Andale Mono' }}>
             /<br />
             The combination is conventionally utilized to represent a single
-            shade of color. the maximum possible characters in a single
-            character is 16: from 0-9 and A-F. In standard #RRGGBB notation,
-            there are thus 256^3 color combinations available, or 16,777,216.
+            shade of color.
+            <br />
+            the maximum possible characters in a single character is 16: from
+            0-9 and A-F.
+            <br />
+            In standard #RRGGBB notation, there are thus 256^3 color
+            combinations available, or 16,777,216.
           </p>
-          <p className="mt-4 text-lg" style={{ fontFamily: 'Andale Mono' }}>
+          <p className="mt-2 text-[16px]" style={{ fontFamily: 'Andale Mono' }}>
             /<br />
             Any data, however, could potentially be remapped to these
-            combinations, beyond colors. The possible amount of unique HexHex is
-            256^3^6 = 2.23e+43, or more than 1 trillion squared.
+            combinations, beyond colors.
+            <br />
+            The possible amount of unique HexHex is 256^3^6 = 2.23e+43, or more
+            than 1 trillion squared.
           </p>
-          <button className="relative flex justify-center items-center w-72 h-20 my-12">
+          <button className="relative flex justify-center items-center w-72 h-20 my-10">
             <div className="absolute left-0 top-0 right-0 bottom-0 bg-black rounded-full filter hover:blur-md"></div>
             <div
-              className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl uppercase"
+              className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[26px] uppercase"
               style={{ fontFamily: 'Andale Mono' }}
             >
               Mint
@@ -65,12 +73,12 @@ const Home = () => {
         </div>
         <div>
           <div
-            className="mb-8 text-center"
+            className="mb-2 text-[16px] text-center"
             style={{ fontFamily: 'Andale Mono' }}
           >
             Example HexHex:
           </div>
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-3 gap-8">
             {new Array(3)
               .fill(null)
               .map(() =>
@@ -78,6 +86,7 @@ const Home = () => {
                   () =>
                     `#${Math.floor(Math.random() * 16 ** 6)
                       .toString(16)
+                      .padStart(6, '0')
                       .toUpperCase()}`,
                 ),
               )
@@ -87,7 +96,7 @@ const Home = () => {
                   className="col-span-3 sm:col-span-1 bg-black h-96 px-10 py-8"
                 >
                   {hexs.map((hex, j) => (
-                    <div key={j} className="mb-2">
+                    <div key={j} className="mb-2 text-[16px]">
                       {hex}
                     </div>
                   ))}
@@ -98,7 +107,7 @@ const Home = () => {
       </main>
 
       <footer
-        className="container mx-auto py-8 text-4xl text-center"
+        className="container mx-auto py-8 text-[26px] text-center"
         style={{ fontFamily: 'Academy Engraved LET' }}
       >
         HexHex
